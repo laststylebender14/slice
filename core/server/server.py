0c0880f798c2bd74a78f673b1c3aa980b1bcea1f
CW-RanjitMahadik <ranjit@carwale.com>
2024-03-02 19:27:38 +0530

    - added command_handler as dependency

--- core/server/server.py
+++ core/server/server.py
@@ -1,16 +1,14 @@
 import socket
 import selectors
 
-from core.storage.kv_store import Store
 from core.server.iserver import IServer
 from core.iomultiplexing.io_multiplexing import IO_Multiplexer
 from core.resp.decoder import decode_redis_command
 from core.commandhandler.command_handler import CommandHandler
-from core.aof.aof import AOF
-from core.resp.encoder import encode_simple_strings_resp, encode_bulk_strings_reps,encode_null_string_resp
+from core.resp.encoder import encode_bulk_strings_reps
 
 class Server(IServer):
-    def __init__(self,store: Store, host:str="localhost",port:int=6379) -> None:
+    def __init__(self,host:str="localhost",port:int=6379,command_handler:CommandHandler = None) -> None:
         self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.server_socket.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1)
         self.server_socket.bind((host,port))
@@ -18,7 +16,7 @@ class Server(IServer):
         self.server_socket.setblocking(False)
         
         # instantiate command_handler to understand resp and forward it to appropriate handler
-        self.command_handler = CommandHandler(store, walFile=AOF("./aof.slice"))
+        self.command_handler = command_handler
         
         # instantiate the io multiplexer
         self.io_multiplexer = IO_Multiplexer(selector= selectors.KqueueSelector(), server_socket=self.server_socket, server=self)
