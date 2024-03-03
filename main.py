09daf86d63485f814a3cf798635551fa02a2a8e3
CW-RanjitMahadik <ranjit@carwale.com>
2024-03-03 17:32:06 +0530

    - clean the main file.

--- main.py
+++ main.py
@@ -1,14 +1,3 @@
-""" 
-TODO: combine io_multiplexer with command handler module.   -> done
-TODO: integrate WAL with command handler.                   -> done
-TODO: implement key expiry sampler                          -> done
----------------------------
-TODO: add test coverage.                                    -> pending
-TODO: integrate RDB file with command handler.              -> pending
-TODO: understand different variations of io/multiplexer.    -> pending
-TODO: add nice logo to cmd on startup                       -> pending
-"""
-
 from core.server.server import Server
 from core.storage.kv_store import KVStore
 from core.aof.aof import AOF_V2
@@ -17,7 +6,7 @@ from core.commandhandler.command_handler import CommandHandler
 
 if __name__ == "__main__":
     walFile = None
-    if True:
+    if False:
         # TODO: enalbe via configuration.
         walFile = AOF_V2("./aof.slice",)
     store = KVStore()
