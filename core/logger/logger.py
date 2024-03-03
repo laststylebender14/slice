d2c101178ef78b4cb1b00e049c0bed87af002c99
CW-RanjitMahadik <ranjit@carwale.com>
2024-03-03 19:07:01 +0530

    - added stream formatter. - export logger method.

--- core/logger/logger.py
+++ core/logger/logger.py
@@ -9,7 +9,17 @@ def set_logging_level(logging_level_str = "debug"):
         logging_level = getattr(logging, logging_level_str.upper(), None)
         if logging_level is not None:
             logger.setLevel(logging_level)
+            
+            console_handler = logging.StreamHandler()
+            formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+            console_handler.setFormatter(formatter)
+            logger.addHandler(console_handler)
+            
         else:
             print("Invalid logging level specified.")
     else:
         print("Logging level not specified.")
+
+
+def get_logger():
+    return logging.getLogger("slice")
