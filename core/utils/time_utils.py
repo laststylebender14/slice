bbea2210543410d27d4915f73ed14d14602ce3f1
CW-RanjitMahadik <ranjit@carwale.com>
2024-03-03 15:29:42 +0530

    - provided proper name for function.

--- core/utils/time_utils.py
+++ core/utils/time_utils.py
@@ -1,11 +1,9 @@
-from core.storage.node import Node
-from core.storage.options import Options
 from time import time
 
 def convert_second_to_ms(time_in_second: int) -> int:
     return time_in_second * 1000
 
-def convert_to_absolute_expiray_in_ms(time_in_second: int) -> int:
+def convert_second_to_absolute_expiray_in_ms(time_in_second: int) -> int:
     return convert_second_to_ms(time()) + convert_second_to_ms(time_in_second)
         
 def convert_time_to_ms() -> int:
