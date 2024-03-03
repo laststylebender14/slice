e7b47d7bf0bde1efaa5efb42adf0672bbb2aadb5
CW-RanjitMahadik <ranjit@carwale.com>
2024-03-03 17:36:04 +0530

    - added banner.

--- main.py
+++ main.py
@@ -4,6 +4,14 @@ from core.aof.aof import AOF_V2
 from core.commandhandler.command_handler import CommandHandler
 
 
+banner = """ 
+ ____  _ _            ____  ____  
+/ ___|| (_) ___ ___  |  _ \| __ ) 
+\___ \| | |/ __/ _ \ | | | |  _ \ 
+ ___) | | | (_|  __/ | |_| | |_) |
+|____/|_|_|\___\___| |____/|____/ 
+"""
+
 if __name__ == "__main__":
     walFile = None
     if False:
@@ -16,5 +24,6 @@ if __name__ == "__main__":
         walFile.replay(CommandHandler(store=store, walFile=None))
         print("[Logger]:","Replying the for AOF file done")
     command_handler = CommandHandler(store=store, walFile=walFile)
+    print(banner)
     Server(store=store,command_handler=command_handler).start_server()
     
