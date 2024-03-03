7883bba1d0cf9603d49f5d455e3717b5b5d3879f
CW-RanjitMahadik <ranjit@carwale.com>
2024-03-03 19:13:05 +0530

    - switched to logger and formatted file.

--- core/iomultiplexing/io_multiplexing.py
+++ core/iomultiplexing/io_multiplexing.py
@@ -3,36 +3,46 @@ import socket
 from time import time
 
 from core.server.iserver import IServer
+from core.logger.logger import get_logger
+
 
 class IO_Multiplexer:
-    def __init__(self,selector: selectors.BaseSelector,server_socket: socket.socket, server: IServer):
+    def __init__(
+        self,
+        selector: selectors.BaseSelector,
+        server_socket: socket.socket,
+        server: IServer,
+    ):
         self.selector = selector
         self.server = server
         self.server_socket = server_socket
         self.concurrent_connection_count = 0
-        self.selector.register(server_socket,selectors.EVENT_READ,self.accept)
-        
-        
+        self.selector.register(server_socket, selectors.EVENT_READ, self.accept)
+
         self.cron_frequency = 1
-        print(f"i/o multiplexer registered with server socket")
-    
+        get_logger().info(f"i/o multiplexer registered with server socket")
+
     def accept(self, client_socket: socket.socket, mask):
         conn, addr = client_socket.accept()
         self.concurrent_connection_count += 1
-        print(f"Accepted connection from {addr}, total connection count : {self.concurrent_connection_count}")
+        get_logger().info(
+            f"Accepted connection from {addr}, total connection count : {self.concurrent_connection_count}"
+        )
         conn.setblocking(False)
         self.selector.register(conn, selectors.EVENT_READ, self.handle_connection)
-                
-    def handle_connection(self,client_socket: socket.socket, mask):
+
+    def handle_connection(self, client_socket: socket.socket, mask):
         data = client_socket.recv(1024).decode().strip()
         if not data:
             self.concurrent_connection_count -= 1
-            print(f"connection terminated from {client_socket}, total connection count : {self.concurrent_connection_count}")
+            get_logger().info(
+                f"connection terminated from {client_socket}, total connection count : {self.concurrent_connection_count}"
+            )
             self.selector.unregister(client_socket)
             client_socket.close()
         else:
-            self.server.handle_connection(client_socket=client_socket,data=data)
-    
+            self.server.handle_connection(client_socket=client_socket, data=data)
+
     def run(self):
         last_cron_exec_time = time()
 
@@ -40,14 +50,14 @@ class IO_Multiplexer:
             if time() - last_cron_exec_time >= self.cron_frequency:
                 self.server.cron_execution()
                 last_cron_exec_time = time()
-                pass                 
+                pass
             events = self.selector.select(-1)
             for key, mask in events:
                 callback = key.data
                 callback(key.fileobj, mask)
-                
+
     def __del__(self):
         """
-            clean up for the selectors
+        clean up for the selectors
         """
         self.selector.unregister(self.server_socket)
