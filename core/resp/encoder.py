431cbb0664b8dcae2c4c7df140d858afa3d0d6c6
CW-RanjitMahadik <ranjit@carwale.com>
2024-03-03 19:14:19 +0530

    - used logger and formatted file

--- core/resp/encoder.py
+++ core/resp/encoder.py
@@ -1,24 +1,28 @@
 from codecs import escape_encode
 
+
 def encode_simple_strings_resp(resp: str = "") -> str:
     if resp == "":
         return encode_null_string_resp()
     return f"+{resp}\r\n".encode()
 
+
 def encode_bulk_strings_reps(resp: str = "") -> str:
     return f"${len(resp)}\r\n{resp}\r\n".encode()
+
 
 def encode_null_string_resp() -> str:
     return "$-1\r\n".encode()
 
+
 def encode_redis_command(command):
     parts = [f"${len(part)}\r\n{part}\r\n" for part in command.split()]
     encoded_command = f"*{len(parts)}\r\n{''.join(parts)}"
-    print(f" encoded command : {encoded_command.encode()}")
     return escape_encode(encoded_command.encode())[0]
+
 
 def encode_integer(value: int) -> str:
     if value >= 0:
-        return b':' + str(value).encode('ascii') + b'\r\n'
+        return b":" + str(value).encode("ascii") + b"\r\n"
     else:
-        return b':-' + str(abs(value)).encode('ascii') + b'\r\n'
+        return b":-" + str(abs(value)).encode("ascii") + b"\r\n"
--- core/storage/kv_store.py
+++ core/storage/kv_store.py
@@ -2,43 +2,56 @@ from abc import ABC, abstractmethod
 
 from core.storage.options import Options
 from core.storage.node import Node
-from core.utils.time_utils import convert_second_to_absolute_expiray_in_ms, convert_time_to_ms
+from core.utils.time_utils import (
+    convert_second_to_absolute_expiray_in_ms,
+    convert_time_to_ms,
+)
 from core.constants.operation_return_constants import StorageOperationReturnType
+from core.logger.logger import get_logger
+
 
 class Store(ABC):
     @abstractmethod
-    def set_nx(self, key: str, value :Node, options: Options) -> int:
+    def set_nx(self, key: str, value: Node, options: Options) -> int:
         pass
+
     @abstractmethod
-    def set_xx(self, key: str, value :Node, options: Options) -> int:
+    def set_xx(self, key: str, value: Node, options: Options) -> int:
         pass
+
     @abstractmethod
-    def set(self, key: str, value :Node, options: Options) -> int:
+    def set(self, key: str, value: Node, options: Options) -> int:
         pass
+
     @abstractmethod
     def get(self, key: str, value: Node) -> Node:
         pass
+
     @abstractmethod
-    def delete(self, key:str) -> bool:
+    def delete(self, key: str) -> bool:
         pass
+
     @abstractmethod
-    def exists(self,key:str) -> bool:
+    def exists(self, key: str) -> bool:
         pass
+
     @abstractmethod
     def store(self):
         pass
 
+
 class KVStore(Store):
     _instance = None
-    def __new__(cls, *args,**kwargs) -> Store:
+
+    def __new__(cls, *args, **kwargs) -> Store:
         if not cls._instance:
             cls._instance = super().__new__(cls)
         return cls._instance
-    
+
     def __init__(self):
         super().__init__()
         self.store: dict[str, Node] = {}
-        
+
     def exists(self, key: str) -> bool:
         if key not in self.store:
             return False
@@ -47,10 +60,10 @@ class KVStore(Store):
             if self.delete(key):
                 return False
         return True
-        
-    def normalize_ttl(self, value:Node|None ,options: Options) -> Node:
-        """ 
-            depending on the options set, this function returns absolute expiry time.
+
+    def normalize_ttl(self, value: Node | None, options: Options) -> Node:
+        """
+        depending on the options set, this function returns absolute expiry time.
         """
         if value.ttl is not None and value.ttl > 0:
             if options == Options.EX:
@@ -58,33 +71,38 @@ class KVStore(Store):
             else:
                 value.ttl = convert_second_to_absolute_expiray_in_ms(value.ttl)
         return value
-        
-        
-    def set_nx(self,key: str, value: Node | None, options:Options = None) -> StorageOperationReturnType:
-        """ set if key doesn't exists"""
+
+    def set_nx(
+        self, key: str, value: Node | None, options: Options = None
+    ) -> StorageOperationReturnType:
+        """set if key doesn't exists"""
         if self.exists(key):
             return StorageOperationReturnType.KEY_VALUE_NOT_INSERTED
-        
-        normalizedValue = self.normalize_ttl(value, options)        
-        print("[Store]:","[setnx]:",f"{key} inserted into db")
+
+        normalizedValue = self.normalize_ttl(value, options)
+        get_logger().info("[Store]:", "[setnx]:", f"{key} inserted into db")
         self.store[key] = normalizedValue
         return StorageOperationReturnType.KEY_VALUE_INSERTED
-    
-    def set_xx(self, key: str, value: Node | None, options:Options = None) -> StorageOperationReturnType:
+
+    def set_xx(
+        self, key: str, value: Node | None, options: Options = None
+    ) -> StorageOperationReturnType:
         if not self.exists(key):
             return StorageOperationReturnType.KEY_VALUE_NOT_INSERTED
-        normalizedValue = self.normalize_ttl(value, options)        
-        print("[Store]:","[set_xx]:",f"{key} inserted into db")
+        normalizedValue = self.normalize_ttl(value, options)
+        get_logger().info("[Store]:", "[set_xx]:", f"{key} inserted into db")
         self.store[key] = normalizedValue
         return StorageOperationReturnType.KEY_VALUE_INSERTED
-        
-    def set(self, key: str, value: Node, options:Options = None) -> StorageOperationReturnType:        
+
+    def set(
+        self, key: str, value: Node, options: Options = None
+    ) -> StorageOperationReturnType:
         if options == Options.NX:
-            return self.set_nx(key,value,options)
+            return self.set_nx(key, value, options)
         if options == Options.XX:
-            return self.set_xx(key,value,options)
+            return self.set_xx(key, value, options)
         else:
-            print("[Store]:","[set]:",f"{key} inserted into db")
+            get_logger().info("[Store]:", "[set]:", f"{key} inserted into db")
             self.store[key] = self.normalize_ttl(value, options)
             return StorageOperationReturnType.KEY_VALUE_INSERTED
 
@@ -92,16 +110,16 @@ class KVStore(Store):
         if self.exists(key):
             return self.store[key]
         return StorageOperationReturnType.KEY_VALUE_NOT_EXISTS
-            
+
     def delete(self, key) -> bool:
         if key in self.store:
             del self.store[key]
-            print("[Store]:",f"{key} deleted from db")
+            get_logger().info("[Store]:", f"{key} deleted from db")
             return True
         return False
-    
+
     def store(self) -> dict[str, Node]:
         return self.store
-    
+
     def __str__(self) -> str:
         return str(self.store)
