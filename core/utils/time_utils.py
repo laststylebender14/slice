64ed8a29d046214b8e8d58d3548d696d27079521
CW-RanjitMahadik <ranjit@carwale.com>
2024-03-03 15:48:40 +0530

    - convert_second_to_ms - always return int.

--- core/utils/time_utils.py
+++ core/utils/time_utils.py
@@ -1,7 +1,7 @@
 from time import time
 
 def convert_second_to_ms(time_in_second: int) -> int:
-    return time_in_second * 1000
+    return int(time_in_second * 1000)
 
 def convert_second_to_absolute_expiray_in_ms(time_in_second: int) -> int:
     return convert_second_to_ms(time()) + convert_second_to_ms(time_in_second)
