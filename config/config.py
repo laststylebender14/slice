885f298f5b82e89591f65dd771a82be148f91455
CW-RanjitMahadik <ranjit@carwale.com>
2024-03-03 19:00:26 +0530

    - set appropriate configuration classes.

--- config/config.py
+++ config/config.py
@@ -1,6 +1,7 @@
 from dotenv import load_dotenv
 import os
 
+from core.logger.logger import set_logging_level
 from config.wal_config import WalConfig
 
 
@@ -9,6 +10,8 @@ from config.wal_config import WalConfig
 
 def load_config(path: str | None = None):
     load_dotenv()
+    
     WalConfig().set_wal_enable(os.getenv("ENABLE_PERSISTENCE"))
     WalConfig().set_path(os.getenv("WAL_FILE_PATH"))
-    print(WalConfig())
+    set_logging_level(os.getenv("LOGGING_LEVEL"))
+    
