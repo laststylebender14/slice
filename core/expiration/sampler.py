a43c9a79b3b2fde823b8fc5d80a794f215c39da6
CW-RanjitMahadik <ranjit@carwale.com>
2024-03-03 14:55:06 +0530

    - created sampler to handle periodic deletion of expired enties.

---
+++ core/expiration/sampler.py
@@ -0,0 +1,43 @@
+from core.storage.node import Node
+
+from random import sample
+
+def expire_sample(dictionary: dict[str, Node]) -> float:
+  """Picks a random sample of keys from a dictionary and calculates the expiration rate.
+
+  Args:
+      dictionary: The dictionary containing Nodes as values.
+
+  Returns:
+      float: The ratio of deleted keys to the sample size (0 if no keys to sample).
+  """
+  
+  sample_key_size = 20
+
+  # Check if there are enough keys for sampling
+  if len(dictionary) < sample_key_size:
+    return 0.0
+
+  # Sample a random set of keys
+  sample_keys = sample(list(dictionary.keys()), sample_key_size)
+
+  # Track deleted keys and iterate over the sample
+  delete_key_cnt = 0
+  for key in sample_keys:
+    val = dictionary.get(key)  # Use get to avoid potential KeyError
+    if val and val.is_node_expired():
+      delete_key_cnt += 1
+      del dictionary[key]
+
+  # Calculate and return the expiration rate
+  return delete_key_cnt / sample_key_size
+
+
+
+def delete_expired_keys(storage):
+    while True:
+        deletion_to_sample_ratio = expire_sample(storage)
+        print("[Sampler]:","sample ratio", deletion_to_sample_ratio)
+        if deletion_to_sample_ratio < 0.25:
+            break
+
