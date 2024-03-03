361d4b7b447a964563dd77188ee29918b4a1ab6c
CW-RanjitMahadik <ranjit@carwale.com>
2024-03-03 19:00:35 +0530

    add logger.

---
+++ core/logger/logger.py
@@ -0,0 +1,15 @@
+import os
+import logging
+
+def set_logging_level(logging_level_str = "debug"):
+    logger = logging.getLogger("slice")
+    logger.disabled = False
+    
+    if logging_level_str:
+        logging_level = getattr(logging, logging_level_str.upper(), None)
+        if logging_level is not None:
+            logger.setLevel(logging_level)
+        else:
+            print("Invalid logging level specified.")
+    else:
+        print("Logging level not specified.")
