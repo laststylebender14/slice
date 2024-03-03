73036d9797964784b7ee1125f12a6ab58ee8c1b7
CW-RanjitMahadik <ranjit@carwale.com>
2024-03-03 19:12:03 +0530

    - formatted.

--- core/logger/logger.py
+++ core/logger/logger.py
@@ -1,20 +1,19 @@
-import os
 import logging
 
-def set_logging_level(logging_level_str = "debug"):
+
+def set_logging_level(logging_level_str="debug"):
     logger = logging.getLogger("slice")
     logger.disabled = False
-    
     if logging_level_str:
         logging_level = getattr(logging, logging_level_str.upper(), None)
         if logging_level is not None:
             logger.setLevel(logging_level)
-            
+
             console_handler = logging.StreamHandler()
-            formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+            formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
             console_handler.setFormatter(formatter)
             logger.addHandler(console_handler)
-            
+
         else:
             print("Invalid logging level specified.")
     else:
