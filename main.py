ad83820d0541172a8f32328b99535d91c978c627
CW-RanjitMahadik <ranjit@carwale.com>
2024-03-03 14:55:39 +0530

    - use AOF_V2 for logging.

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 """ 
 TODO: combine io_multiplexer with command handler module.   -> done
 TODO: integrate WAL with command handler.                   -> done
-TODO: implement key expiry sampler                          -> started
+TODO: implement key expiry sampler                          -> done
 ---------------------------
 TODO: add test coverage.                                    -> pending
 TODO: integrate RDB file with command handler.              -> pending
@@ -11,15 +11,15 @@ TODO: add nice logo to cmd on startup                       -> pending
 
 from core.server.server import Server
 from core.storage.kv_store import KVStore
-from core.aof.aof import AOF
+from core.aof.aof import AOF_V2
 from core.commandhandler.command_handler import CommandHandler
 
 
 if __name__ == "__main__":
     walFile = None
-    if False:
+    if True:
         # TODO: enalbe via configuration.
-        walFile = AOF("./aof.slice",)
+        walFile = AOF_V2("./aof.slice",)
     store = KVStore()
     if walFile:
         print("[Logger]:","Checking for AOF file")
@@ -27,5 +27,5 @@ if __name__ == "__main__":
         walFile.replay(CommandHandler(store=store, walFile=None))
         print("[Logger]:","Replying the for AOF file done")
     command_handler = CommandHandler(store=store, walFile=walFile)
-    Server(command_handler=command_handler).start_server()
+    Server(store=store,command_handler=command_handler).start_server()
     
