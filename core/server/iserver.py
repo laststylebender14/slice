3bd34c50fa9544fc53269850afa930cb8ee01ef9
CW-RanjitMahadik <ranjit@carwale.com>
2024-03-04 13:22:49 +0530

    - remove default constructor.

--- core/server/iserver.py
+++ core/server/iserver.py
@@ -2,16 +2,14 @@ from abc import ABC, abstractmethod
 import socket
 
 class IServer(ABC):
-    def __init__(self,host:str="localhost",port:int=6379) -> None:
-        pass
     @abstractmethod
-    def handle_connection(self, client_socket: socket.socket,data: any):
+    def handle_connection(self, client_socket: socket.socket, data: any):
         pass
-    
+
     @abstractmethod
     def start_server(self):
         pass
-    
+
     @abstractmethod
     def cron_execution(self):
         pass
