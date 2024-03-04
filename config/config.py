d6a5d4a74b3ba22b4d22e1fdd9e1d53d44736d6a
CW-RanjitMahadik <ranjit@carwale.com>
2024-03-04 13:22:38 +0530

    - set logging format before loading any configuration.

--- config/config.py
+++ config/config.py
@@ -3,15 +3,14 @@ import os
 
 from core.logger.logger import set_logging_level
 from config.wal_config import WalConfig
+from config.server_config import ServerConfig
 
 
-
-
-
-def load_config(path: str | None = None):
+def load_config():
     load_dotenv()
-    
-    WalConfig().set_wal_enable(os.getenv("ENABLE_PERSISTENCE"))
-    WalConfig().set_path(os.getenv("WAL_FILE_PATH"))
-    set_logging_level(os.getenv("LOGGING_LEVEL"))
-    
+    set_logging_level(os.getenv("LOGGING_LEVEL", "info"))
+
+    WalConfig().set_wal_enable(os.getenv("ENABLE_PERSISTENCE", False))
+    WalConfig().set_path(os.getenv("WAL_FILE_PATH", "./aof.slice"))
+    ServerConfig().set_host(os.getenv("HOST", "localhost"))
+    ServerConfig().set_port(os.getenv("PORT", 6379))
